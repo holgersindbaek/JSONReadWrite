9ea62b9e7eac3f20fa8172261f40eeb2785eb552
holgersindbaek <holgersindbaek@gmail.com>
2013-07-01 20:04:46 -0400

    changed the way you save json

--- lib/json_read_write/json_read_write.rb
+++ lib/json_read_write/json_read_write.rb
@@ -48,9 +48,10 @@ module JSONReadWrite
   # 
   # Raises NoFileError if the json file doesn't exist
   def self.updateJSONFileWithObject(fileName, object)
-    # raise ArgumentError, "Expected object to be an instance of Array or Hash, was #{object.class}" unless object.is_a?(Array) || object.is_a?(Hash)
+    raise ArgumentError, "Expected object to be an instance of Array or Hash, was #{object.class}" unless object.is_a?(Array) || object.is_a?(Hash)
     raise NoFileError, "Cannot find json file: #{fileName}.json in documents directory" unless exist?(fileName)
-    object.writeToFile(jsonPath(fileName), atomically: true)
+    # object.writeToFile(jsonPath(fileName), atomically: true)
+    object.to_s.writeToFile(path, atomically:true, encoding:NSASCIIStringEncoding, error:nil)
   end
 
   # The object containing the data stored in the json
